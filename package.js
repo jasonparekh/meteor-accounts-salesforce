5529cabf28abed7219c963ead340909d1c1578ec
Jason Parekh <jasonparekh@gmail.com>
2014-10-13 13:37:20 -0400

    Update for Meteor package

--- package.js
+++ package.js
@@ -1,11 +1,14 @@
 Package.describe({
-  summary: "Accounts service for Salesforce accounts"
+  summary: "Accounts service for Salesforce accounts",
+  version: "0.0.3",
+  git: "https://github.com/jasonparekh/meteor-accounts-salesforce.git"
 });
 
-Package.on_use(function(api) {
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.3.1');
   api.use('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('salesforce', ['client', 'server']);
+  api.use('saleswise:salesforce@0.0.3', ['client', 'server']);
 
   api.add_files(['salesforce_login_button.css'], 'client');
 
